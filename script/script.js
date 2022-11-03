8a83571533a73b3b67ec11ce639e3b81e13e2fbe
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-11-03 20:32:30 +0300

    Add single page app functionality

--- script/script.js
+++ script/script.js
@@ -22,3 +22,38 @@ if (localStorage.getItem('formData')) {
   titleInput.value = formValueObj.title;
   authorInput.value = formValueObj.author;
 }
+
+
+const bookLink = document.getElementById("library");
+const bookStore = document.getElementById("bookStore");
+const addBook = document.getElementById("addBook");
+const addLink = document.getElementById("addLink");
+const contactLink = document.getElementById("contactLink");
+const contact = document.getElementById("contact");
+const btn1 = document.getElementById("add-btn1")
+
+bookLink.addEventListener("click", () => {
+  bookStore.style.display = "block";
+  addBook.style.display = "none";
+  contact.style.display = "none";
+});
+
+bookStore.addEventListener("click", () => {
+  addBook.style.display = "block";
+  bookList.style.display = "none";
+  contact.style.display = "none";
+  bookStore.style.display = "none";
+});
+
+contactLink.addEventListener("click", () => {
+  contact.style.display = "block";
+  bookStore.style.display = "none";
+  addBook.style.display = "none";
+});
+
+addLink.addEventListener("click", () => {
+  addBook.style.display = "block",
+  contact.style.display = "none";
+  bookStore.style.display = "none";
+});
+
