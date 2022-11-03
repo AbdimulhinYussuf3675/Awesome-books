c3d4d2b02a13a4fcd9ed8ced0ca626ca1457e5f3
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-11-03 09:20:31 +0300

    Add jasvascript classes

--- script/script.js
+++ script/script.js
@@ -1,76 +1,24 @@
-const booksList = [
-  {
-    title: 'Kidagaa kimemwozea',
-    author: 'Ken Walibora',
-    id: 0,
-  },
-  {
-    title: 'Tumbo Lisiloshiba',
-    author: 'Abdimulhin Adan',
-    id: 1,
-  },
-  {
-    title: 'The River And The Source',
-    author: 'Margaret A. Ogola',
-    id: 2,
-  },
-];
-
-function createBook(book) {
-  const booksStore = document.getElementById('booksStore');
-  const bookDiv = document.createElement('div');
-  bookDiv.className = 'book';
-  bookDiv.id = `book-${book.id}`;
-  const bookElement = `
-        <h4 class="bookTitle">${book.title}</h4>
-        <p class="bookAuthor">${book.author}</p>
-        <button class="removeBtn">Remove</button>
-        <hr>
-      `;
-  bookDiv.innerHTML += bookElement;
-  bookDiv.querySelector('.removeBtn').addEventListener('click', (event) => {
-    const book = document.getElementById(event.target.parentElement.id);
-    book.parentElement.removeChild(book);
-    let storedData = JSON.parse(localStorage.getItem('storedData'));
-    storedData = storedData.filter((bookobj) => {
-      if (bookobj.id.toString() === book.id.substring(5, book.id.length)) {
-        return false;
-      }
-      return true;
-    });
-    localStorage.setItem('storedData', JSON.stringify(storedData));
-  });
-  booksStore.append(bookDiv);
-}
-
-function storeInputData() {
-  const inputData = {
-    title: document.getElementById('title').value,
-    author: document.getElementById('author').value,
-    id: Math.floor(Math.random() * 10000),
-  };
-  const storedData = JSON.parse(localStorage.getItem('storedData'));
-  storedData.push(inputData);
-  localStorage.setItem('storedData', JSON.stringify(storedData));
-  createBook(inputData);
-}
-
-const initLoad = (booksList) => {
-  const storedData = JSON.parse(localStorage.getItem('storedData'));
-  if (storedData) {
-    storedData.forEach((book) => {
-      createBook(book);
-    });
-  } else {
-    localStorage.setItem('storedData', JSON.stringify(booksList));
-    booksList.forEach((book) => {
-      createBook(book);
-    });
-  }
-  const addBtn = document.getElementById('addBtn');
-  addBtn.addEventListener('click', () => {
-    storeInputData();
-  });
+const formData = {
+  title: '',
+  author: '',
 };
 
-initLoad(booksList);
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+
+titleInput.addEventListener('input', () => {
+  formData.title = titleInput.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+authorInput.addEventListener('input', () => {
+  formData.author = authorInput.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+if (localStorage.getItem('formData')) {
+  const formValue = localStorage.getItem('formData');
+  const formValueObj = JSON.parse(formValue);
+  titleInput.value = formValueObj.title;
+  authorInput.value = formValueObj.author;
+}
